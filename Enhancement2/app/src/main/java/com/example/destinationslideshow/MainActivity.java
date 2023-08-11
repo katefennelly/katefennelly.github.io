7ad81587cb36f0461e48b8b5c7cebc6585243dae
Kate Kowalyshyn <92118710+katekowalyshyn@users.noreply.github.com>
2023-08-11 19:04:30 -0400

    Create MainActivity.java

---
+++ Enhancement2/app/src/main/java/com/example/destinationslideshow/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.destinationslideshow;
+
+import android.database.CursorWindow;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.File;
+import java.lang.reflect.Field;
+
+import com.example.destinationslideshow.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String categoryToShow = "ALL";
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+       setSupportActionBar(binding.toolbar);
+       getSupportActionBar().hide();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        try {
+            Field field = CursorWindow.class.getDeclaredField("sCursorWindowSize");
+            field.setAccessible(true);
+            field.set(null, 100 * 1024 * 1024); //the 100MB is the new size
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void setCategoryToShow(String category) {
+        categoryToShow = category;
+    }
+
+    public String categoryToShow() {
+        return categoryToShow;
+    }
+}
