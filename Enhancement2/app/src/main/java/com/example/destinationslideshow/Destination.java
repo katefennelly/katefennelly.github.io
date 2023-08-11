64aea1159c00debcd9f5750a96af28d030f06dd7
Kate Kowalyshyn <92118710+katekowalyshyn@users.noreply.github.com>
2023-08-11 19:03:53 -0400

    Create Destination.java

---
+++ Enhancement2/app/src/main/java/com/example/destinationslideshow/Destination.java
@@ -0,0 +1,38 @@
+package com.example.destinationslideshow;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Destination {
+
+    //initialize variables
+    private String title;
+    private String description;
+
+    private String category;
+
+    private Bitmap image;
+//    private int imageResource;
+
+    public Destination(String title, String description, String category, byte[] imageData){
+        this.title = title;
+        this.description = description;
+        this.category = category;
+
+        //convert image to bitmap
+        if (imageData != null) {
+            this.image = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+        }
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+}
